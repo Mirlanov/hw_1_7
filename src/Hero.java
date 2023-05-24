e887efa06fd54e1a179b08d19e0f213ba3caeba9
Mirlanov <mirlanovdamir@gmail.com>
2023-05-24 16:59:08 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,10 @@
+public abstract class Hero implements HavingSuperAbility {
+
+    public static int health;
+    public static int damage;
+
+    @Override
+    public void applySuperAbility() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        HavingSuperAbility[] superAbilities = {new Warrior(), new Magic(), new Medic()};
+        for (int i = 0; i < superAbilities.length; i++) {
+            superAbilities[i].applySuperAbility();
+        }
+
+    }
+}
